1f0dc03aad563f0e42aa4c4a602ce7a522f684d7
hdw96 <hdw9610@naver.com>
2020-05-31 15:07:41 +0900

    Extract Subclass
    1. Extract Subclass
    2. Zombie class -> RegularZobie, DogZombie, TinyZombie class
    3. In moan function of Zombie class, each object is classified through
    the type variable,
    I thought this should be implemented separately in each class.

---
+++ GenericZombieShooter/src/genericzombieshooter/actors/RegularZombie.java
@@ -0,0 +1,41 @@
+package genericzombieshooter.actors;
+
+
+import genericzombieshooter.misc.Globals;
+import genericzombieshooter.misc.Sounds;
+import genericzombieshooter.structures.Animation;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+
+
+public class RegularZombie extends Zombie{
+
+	public RegularZombie(Point2D.Double p_, int type_, int health_, int damage_, double speed_, int cash_, int exp_,
+			Animation animation_) {
+		super(p_, Globals.ZOMBIE_REGULAR_TYPE, health_, damage_, speed_, cash_, exp_, animation_);
+		
+	}
+
+	@Override
+    public void draw(Graphics2D g2d) {
+        super.draw(g2d);
+        
+    }
+    
+	@Override
+    public void moan(Player player) {
+        if(!this.moaned) {
+            if(Globals.gameTime.getElapsedMillis() >= this.nextMoan) {
+                double xD = player.getCenterX() - this.x;
+                double yD = player.getCenterY() - this.y;
+                double dist = Math.sqrt((xD * xD) + (yD * yD));
+                double gain = 1.0 - (dist / Player.AUDIO_RANGE);
+                Sounds.MOAN1.play(gain);
+                this.moaned = true;
+            }
+        }
+    }
+}
+
