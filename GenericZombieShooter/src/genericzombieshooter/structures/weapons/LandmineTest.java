77a7832e00e3d8be4680cc39cd615aa9e9ca4479
Se Beom Park <znznz6037@gmail.com>
2020-06-21 21:17:03 +0900

    LandmineTest
    Testing Landmine Constructor

---
+++ GenericZombieShooter/src/genericzombieshooter/structures/weapons/LandmineTest.java
@@ -0,0 +1,76 @@
+package genericzombieshooter.structures.weapons;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.awt.geom.Point2D;
+
+import org.junit.Test;
+
+import genericzombieshooter.actors.Player;
+import kuusisto.tinysound.TinySound;
+
+public class LandmineTest {
+	private static Landmine landmine;
+	Player player = new Player(100, 100, 48, 48);
+	Point2D.Double pos = new Point2D.Double(100, 48);
+	
+	/**
+	*Purpose: Constructor test
+	*Input: Landmine() constructor
+	*Expected: Not Null
+	*/
+	@Test
+	public void constructorTest() {
+		landmine = new Landmine();
+		assertNotNull(landmine);
+	}
+	
+	/**
+	*Purpose: Check weapon price of Landmine
+	*Input: getWeaponPrice()
+	*Expected:
+	*	Return 750
+	*/
+	@Test
+	public void getWeaponPriceTest() {
+		int weaponPrice = landmine.getWeaponPrice();
+		assertEquals(750, weaponPrice);
+	}
+	
+	/**
+	*Purpose: Check ammo price of Landmine
+	*Input: getAmmoPrice()
+	*Expected:
+	*	Return 400
+	*/
+	@Test
+	public void getAmmoPriceTest() {
+		int ammoPrice = landmine.getAmmoPrice();
+		assertEquals(400, ammoPrice);
+	}
+	
+	/**
+	*Purpose: Check ammo pack amount of Landmine
+	*Input: getAmmoPackAmount()
+	*Expected:
+	*	Return 1
+	*/
+	@Test
+	public void getAmmoPackAmountTest() {
+		int ammoPackAmount = landmine.getAmmoPackAmount();
+		assertEquals(1, ammoPackAmount);
+	}
+	
+	/**
+	*Purpose: Check default ammo of Landmine
+	*Input: resetAmmo()
+	*Expected:
+	*	Return 1
+	*/
+	@Test
+	public void resetAmmoTest() {
+		landmine.resetAmmo();
+		assertEquals(1, landmine.getAmmoLeft());
+	}
+}
